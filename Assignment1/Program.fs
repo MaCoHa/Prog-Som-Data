4945f7f81021713ebbab99728048421b9e6efdc6
Jack Kryger Sørensen <74965501+jeroleski@users.noreply.github.com>
2022-09-01 14:47:31 +0200

    More tests for Intro2

--- Assignment1/Program.fs
+++ Assignment1/Program.fs
@@ -19,6 +19,30 @@ test "min" (eval (expr.Prim("min", expr.CstI 3, expr.CstI 5)) emptyenv) 3
 test "eqal" (eval (expr.Prim("==", expr.CstI 3, expr.CstI 5)) env) 0
 test "not equal" (eval (expr.Prim("==", expr.CstI 5, expr.CstI 5)) env) 1
     
+test "fmt a1" (fmt a1) "(v - (w + z))"
+test "fmt a2" (fmt a2) "(2 * (v - (w + z)))"
+test "fmt a3" (fmt a3) "(x + (y + (z + v)))"
+
+test "simplify add 0 left"  (simplify <| Add(CstI 0, Var "x")) (Var "x")
+test "simplify add 0 right" (simplify <| Add(Var "x", CstI 0)) (Var "x")
+test "simplify add 0 both"  (simplify <| Add(CstI 0, CstI 0)) (CstI 0)
+test "simplify add cons"    (simplify <| Add(CstI 4, CstI 9)) (CstI 13)
+test "simplify add vars"    (simplify <| Add(CstI 4, Var "x")) (Add(CstI 4, Var "x"))
+
+test "simplify sub 0 left"  (simplify <| Sub(CstI 0, Var "x")) (Sub(CstI 0, Var "x"))
+test "simplify sub 0 right" (simplify <| Sub(Var "x", CstI 0)) (Var "x")
+test "simplify sub eq vars" (simplify <| Sub(CstI 0, CstI 0)) (CstI 0)
+test "simplify sub 2 cons"  (simplify <| Sub(Var "x", Var "x")) (CstI 0)
+test "simplify sub cons"    (simplify <| Sub(CstI 4, CstI 9)) (CstI -5)
+test "simplify sub vars"    (simplify <| Sub(CstI 4, Var "x")) (Sub(CstI 4, Var "x"))
+
+test "simplify mul 1 left"  (simplify <| Mul(CstI 1, Var "x")) (Var "x")
+test "simplify mul 1 right" (simplify <| Mul(Var "x", CstI 1)) (Var "x")
+test "simplify mul 1 both"  (simplify <| Mul(CstI 1, CstI 1)) (CstI 1)
+test "simplify mul 0 1"     (simplify <| Mul(CstI 0, CstI 1)) (CstI 0)
+test "simplify mul cons"    (simplify <| Mul(CstI 4, CstI 9)) (CstI 36)
+test "simplify mul vars"    (simplify <| Mul(CstI 4, Var "x")) (Mul(CstI 4, Var "x"))
+
 
 
 
