12ad9fba678aaaf1ba6aac50bc4b699d2417bb31
Jack Kryger Sørensen <74965501+jeroleski@users.noreply.github.com>
2022-09-01 15:51:08 +0200

    More tests for Intro2

--- Assignment1/Program.fs
+++ Assignment1/Program.fs
@@ -1,8 +1,8 @@
-﻿open Intro2
-
-let printSuccess = true
+﻿open Incomp1
+open Intro2
 
 //testing function
+let printSuccess = true
 let test case val1 val2 =
     if val1 = val2
     then
@@ -10,19 +10,33 @@ let test case val1 val2 =
         then printfn "SUCCESS: '%A' evaluates to '%A'" case val1
     else printfn "FAILURE: '%A' has inconsistency between '%A' and '%A'" case val1 val2
 
-test "eval e1" (eval e1 env) 17
-test "eval e2" (eval e2 env) 6
-test "eval e3" (eval e3 env) 1002
 
+
+//exercise 1
+
+//exe 1.
+
+//exe 1.1.i - 1.1.ii
 test "max" (eval (expr.Prim("max", expr.CstI 3, expr.CstI 5)) emptyenv) 5
 test "min" (eval (expr.Prim("min", expr.CstI 3, expr.CstI 5)) emptyenv) 3
 test "eqal" (eval (expr.Prim("==", expr.CstI 3, expr.CstI 5)) env) 0
 test "not equal" (eval (expr.Prim("==", expr.CstI 5, expr.CstI 5)) env) 1
-    
+
+//exe 1.1.iii
+test "eval e1" (eval e1 env) 17
+test "eval e2" (eval e2 env) 6
+test "eval e3" (eval e3 env) 1002
+
+//exe 1.1.iv - 1.1.vi
+test "if true"  (eval (expr.If(expr.CstI 1, expr.CstI 3, expr.CstI 5)) env) 3
+test "if false" (eval (expr.If(expr.CstI 0, expr.CstI 3, expr.CstI 5)) env) 5
+
+//exe 1.2.i - 1.2.iii
 test "fmt a1" (fmt a1) "(v - (w + z))"
 test "fmt a2" (fmt a2) "(2 * (v - (w + z)))"
 test "fmt a3" (fmt a3) "(x + (y + (z + v)))"
 
+//exe 1.2.iv
 test "simplify add 0 left"  (simplify <| Add(CstI 0, Var "x")) (Var "x")
 test "simplify add 0 right" (simplify <| Add(Var "x", CstI 0)) (Var "x")
 test "simplify add 0 both"  (simplify <| Add(CstI 0, CstI 0)) (CstI 0)
