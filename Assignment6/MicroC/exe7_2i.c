009df736fa3f73be8b0dc31ffccde9115fb55560
Jack Kryger Sørensen <74965501+jeroleski@users.noreply.github.com>
2022-10-27 11:26:50 +0200

    added answers.txt, and solved 7.2.i

---
+++ Assignment6/MicroC/exe7_2i.c
@@ -0,0 +1,29 @@
+﻿void main(int n)
+{
+    int arr[4];
+    arr[0] = 7;
+    arr[1] = 13;
+    arr[2] = 9;
+    arr[3] = 8;
+    int sum;
+    
+    arrsum(n, arr, &sum);
+    print sum;
+    println;
+}
+
+void arrsum(int n, int arr[], int *sump)
+{
+    int i;
+    i = 0;
+    int sum;
+    sum = 0;
+    
+    while (i <= n)
+    {
+        sum = sum + arr[i];
+        i = i + 1;
+    }
+    
+    *sump = sum;
+}
