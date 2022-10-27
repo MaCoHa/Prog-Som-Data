9a26bca1c2ac4777e5890ace46851e4bc4508794
Jack Kryger Sørensen <74965501+jeroleski@users.noreply.github.com>
2022-10-27 17:44:34 +0200

    exe 8.3 done

--- Assignment7/MicroC/exe8_3.c
+++ Assignment7/MicroC/exe8_3.c
@@ -1,16 +1,14 @@
 ﻿void main(int n)
 {
-    int i;
-    i = 2;
     int arr[4];
     arr[0] = 0;
     arr[1] = 1;
     arr[2] = 2;
     arr[3] = 3;
 
-    ++(arr[++i]);
+    ++arr[++n];
 
-    print i;
-    print arr[i];
+    print n;
+    print arr[n];
     println;
 }
