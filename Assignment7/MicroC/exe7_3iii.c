6184c03effdd99ec7bff2a2adaee0b823d73de05
Jack Kryger Sørensen <74965501+jeroleski@users.noreply.github.com>
2022-10-27 13:59:39 +0200

    initial setup for assignment 7

---
+++ Assignment7/MicroC/exe7_3iii.c
@@ -0,0 +1,39 @@
+﻿void main(int n){
+    int arr[7];
+    arr[0] = 1;
+    arr[1] = 2;
+    arr[2] = 1;
+    arr[3] = 1;
+    arr[4] = 1;
+    arr[5] = 2;
+    arr[6] = 0;
+    int freq[4];
+    freq[0] = 0;
+    freq[1] = 0;
+    freq[2] = 0;
+    freq[3] = 0;
+    histogram(n, arr, 3, freq);
+    
+    println;
+    print freq[0];
+    print freq[1];
+    print freq[2];
+    print freq[3];
+}
+//if array is too small, compiler will execute exception of key  not found
+
+
+// when done --> 
+// 1. dublicate the file
+// 2. rename to exe7_3iii.c
+// 3. change while-loops to for-loops
+void histogram(int n, int ns[], int max, int freq[])
+{
+    int i;
+    for (i = 0; i < n; i = i + 1)
+    {
+        int x;
+        x = ns[i];
+        freq[x] = freq[x] + 1;
+    }
+}
---
+++ Assignment7/MicroC/exe7_5.c
@@ -0,0 +1,14 @@
+﻿void main(int n)
+{
+    int np;
+    np = ++n;
+    --n;
+    int nm;
+    nm = --n;
+    ++n;
+
+    print nm;
+    print n;
+    print np;
+    println;
+}
