4a6c92cc36d08eac1c8f9fb2a728465aaeebb000
Jack Kryger Sørensen <74965501+jeroleski@users.noreply.github.com>
2022-10-27 11:52:24 +0200

    exe 7.2ii

---
+++ Assignment6/MicroC/exe7_2ii.c
@@ -0,0 +1,35 @@
+﻿void main(int n)
+{
+    int arr[20];
+    squares(n, arr);
+
+    int sum;
+    arrsum(n, arr, &sum);
+    print sum;
+    println;
+}
+
+void squares(int n, int arr[])
+{
+    n = n - 1;
+    while (n >= 0)
+    {
+        arr[n] = n * n;
+        n = n - 1;
+    }
+}
+
+void arrsum(int n, int arr[], int *sump)
+{
+    n = n - 1;
+    int sum;
+    sum = 0;
+    
+    while (n >= 0)
+    {
+        sum = sum + arr[n];
+        n = n - 1;
+    }
+    
+    *sump = sum;
+}
